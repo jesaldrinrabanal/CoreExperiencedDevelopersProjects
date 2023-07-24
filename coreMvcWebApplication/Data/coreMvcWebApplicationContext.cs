91d1f9046e3e5d94c134492f42cf5632e419fa09
ARISmobileapp <123352929+jesaldrinrabanal@users.noreply.github.com>
2023-07-24 19:04:42 +0800

    Added Dependency Injection (coreMvcWebApplication)

---
+++ coreMvcWebApplication/Data/coreMvcWebApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using coreMvcWebApplication.Models;
+
+namespace coreMvcWebApplication.Data
+{
+    public class coreMvcWebApplicationContext : DbContext
+    {
+        public coreMvcWebApplicationContext (DbContextOptions<coreMvcWebApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<coreMvcWebApplication.Models.User> User { get; set; } = default!;
+    }
+}
