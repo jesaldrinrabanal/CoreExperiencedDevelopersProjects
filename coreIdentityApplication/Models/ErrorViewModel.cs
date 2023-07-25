791b68a88125f9c9de8f19647cd39082220dd08c
Jes Aldrin Rabanal <rabanalaldrin@gmail.com>
2023-07-25 15:49:34 +0800

    Added Identity Application

---
+++ coreIdentityApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace coreIdentityApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
