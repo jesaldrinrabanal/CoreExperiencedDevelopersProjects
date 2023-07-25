a90c62cb78bdf79f6d79ed76f1b3b58db620c4e5
Jes Aldrin Rabanal <rabanalaldrin@gmail.com>
2023-07-25 20:05:53 +0800

    Added Route

---
+++ AspNetCoreWebAPIProject/Controllers/EmployeesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace AspNetCoreWebAPIProject.Controllers
+{
+    // [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        /* // Route
+        [Route("employees/all")]
+        public string GetAllEmployees()
+        {
+            return "All Employees Here";
+        }
+        [Route("employees/{id:int:min(100)}")]
+        public string GetEmployeeById(int id)
+        {
+            return "Employee with Id: " + id;
+        }
+        [Route("emps/{id:int}")]
+        public string GetEmployeeDetails(int id)
+        {
+            return "Employee Details: " + id;
+        }
+        [Route("emps/{name:alpha}")]
+        public string GetEmployeeDetails(string name)
+        {
+            return "Employee Details: " + name;
+        }
+        */
+    }
+}
